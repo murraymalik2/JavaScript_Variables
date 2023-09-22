5cd4c5023b37b816e804a4dad3abf5bac1fae9ec
murra <murraymalik2@gmail.com>
2023-09-22 09:57:40 -0400

    javascript

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Frodo";
+let lastName = "Baggins";
+var age = 51;
 // Exercise 2
-
+lastName = "Gamgee";
+age = 52;
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first release in ${createdYear}.');
+
+let product = "T-shirty";
+let price = 19.99;
+let isOnSale = false;
+let salePercentge = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log('${product} in size ${selectedSize} is $${price} with ${stock} in stock.');
+let title = " Name of the Wind";
+let author = " Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log('${title} by ${authot} is ${pageCount} pages long. Your last bookmark was page ${bookmark]');
